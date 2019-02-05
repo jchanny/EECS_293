51e01b7243e76d6e34d6af8773dc7a273755b741
jeremy <jsc126@case.edu>
2019-02-04 20:20:36 -0500

    hw3 init

---
+++ hw3/edu/cwru/jsc126_law136/parser/InternalNode.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cwru.jsc126_law136.parser;
+import java.util.*;
+
+/**
+ *
+ * @author wildm
+ */
+public final class InternalNode implements Node {
+    
+    private final List<Node> children;
+    
+    //constructor
+    private InternalNode(List<Node> kids)
+    {
+        children = kids;
+    }
+    
+    //children getter
+    public List<Node> getChildren()
+    {
+        return children;
+    }
+    
+    public List<Token> toList()
+    {
+        List<Token> internal = new LinkedList<Token>();
+        for (Node element: children){
+            for(Token t : element.toList()){
+                internal.add(t);
+            }
+        }
+        return internal;
+    }
+
+    public static InternalNode build(List<Node> attach){
+        if(attach == null){
+            throw new NullPointerException("Children nodes are null.");
+        }
+        return new InternalNode(attach);
+    }
+
+    private String output = null;
+    //String for toString method
+    public String toString()
+    {
+        if (output != null)
+        {
+            for (Node element: children)
+                output += "[" + (element.toString()) + "]";
+            
+            return output;
+        }
+        //method previously invoked
+        else
+            return output;
+    }    
+}
