b5d7dab9f9af4ce39a1e7361cc161aa969eea19c
jeremy <jsc126@case.edu>
2019-02-05 12:05:50 -0500

    SymbolSequence init

--- hw3/parser/SymbolSequence.java
+++ hw3/parser/SymbolSequence.java
@@ -3,25 +3,37 @@
   Class for building children of an internal node*/
 package parser;
 
+import java.util.ArrayList;
+
 final class SymbolSequence{
 
-    private final List<Symbol> production;
+    private final List <Symbol> production;
 	
-    private SymbolSequence(){
-	
+    private SymbolSequence(List <Symbol> production){
+	this.production = production;
     }
 
-    static final SymbolSequence build(){
-	
+    static final SymbolSequence build(List <Symbol> production){
+	if(production == null)
+	    throw new NullPointerException("Input is null.");
+
+	return new SymbolSequence(production);
     }
 
-    ParseState match(List<Token> input){
+    static final SymbolSequence build(Symbol ... symbols){
+	
+    }
+    
+    ParseState match(List <Token> input){
 	if(input == null){
 	    throw new NullPointerException("Input is null.");
 	}
+	List <Token> remainderList = input;
+	List <Token> children = new ArrayList<Token>();
+	
     }
 
     String toString(){
-	
+	return production.toString();
     }
 }
