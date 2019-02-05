588912b7e8ecd7ede484176d46277a5be20862fd
jeremy <jsc126@case.edu>
2019-02-05 16:01:33 -0500

    symbolsequence complete

--- hw3/parser/SymbolSequence.java
+++ hw3/parser/SymbolSequence.java
@@ -7,30 +7,53 @@ import java.util.ArrayList;
 
 final class SymbolSequence{
 
-    private final List <Symbol> production;
-	
-    private SymbolSequence(List <Symbol> production){
+    private final List<Symbol> production;
+
+    static final SymbolSequence EPSILON = new SymbolSequence(new ArrayList<Symbol>());
+    
+    private SymbolSequence(List<Symbol> production){
 	this.production = production;
     }
 
-    static final SymbolSequence build(List <Symbol> production){
+    /*Takes in a list of Symbols and creates a new SymbolSequence*/
+    static final SymbolSequence build(List<Symbol> production){
 	if(production == null)
 	    throw new NullPointerException("Input is null.");
 
 	return new SymbolSequence(production);
     }
 
+    /*Build method that takes variable number of Sybmols*/
     static final SymbolSequence build(Symbol ... symbols){
-	
-    }
-    
-    ParseState match(List <Token> input){
-	if(input == null){
+	if(symbols == null)
 	    throw new NullPointerException("Input is null.");
-	}
-	List <Token> remainderList = input;
-	List <Token> children = new ArrayList<Token>();
 	
+	List<Symbol> prod = new ArrayList<Symbol>();
+	for(Symbol sym : symbols){
+	    prod.add(sym);
+	}
+
+	return new SymbolSequence(prod);
+    }
+
+    /*Builds a new ParseState from a list of Tokens*/
+    ParseState match(List<Token> input){
+	if(input == null)
+	    throw new NullPointerException("Input is null.");
+	
+	List<Token> remainderList = input;
+	List<Token> children = new ArrayList<Token>();
+	
+	for(Symbol sym : production){
+	    ParseState result = sym.parse(remainderList);
+	    if(result == ParseState.FAILURE)
+		return failure;
+	    //else
+	    children.add(result);
+	    remainderList = result.getRemainder();
+	}
+
+	return ParseState.build(new InternalNode(children), remainderList);
     }
 
     String toString(){
