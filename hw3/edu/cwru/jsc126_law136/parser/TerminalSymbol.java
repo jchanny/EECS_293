74a01c2acdf32b5062d9cb6157aaf34a2bca6584
jeremy <jsc126@case.edu>
2019-02-04 20:23:17 -0500

    this package thing is nasty

--- hw3/edu/cwru/jsc126_law136/parser/TerminalSymbol.java
+++
@@ -1,9 +0,0 @@
-/*Jeremy Chan jsc126
-  Lennox Wildman law136
-  enum class
-*/
-package edu.cwru.jsc126_law136.parser;
-
-public enum TerminalSymbol{
-    VARIABLE, PLUS, MINUS, TIMES, DIVIDE, OPEN, CLOSE;
-}
